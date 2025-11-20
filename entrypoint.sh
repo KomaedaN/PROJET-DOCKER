1045a1284ea9b189250322eb372c2a1e849f8609
Thomas <84379061+KomaedaN@users.noreply.github.com>
2025-11-20 19:11:12 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,9 @@
 set -e
 
 cd /var/www/html
-cp -n /var/www/html/.env1 /var/www/html/.env
-php-fpm8.1 -F
 
 if ! grep -qE "^APP_KEY=base64:" .env; then
     php artisan key:generate --force && php artisan migrate:fresh --seed
 fi
-
 
 exec "$@"
